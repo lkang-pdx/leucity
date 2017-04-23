fb43e7870666cf8747f55077a906a8e789512f11
Luna Kang <luna.kang.ca@gmail.com>
2017-04-23 13:21:59 -0700

    Search Component

---
+++ src/app/search-users/search-users.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-search-users',
+  templateUrl: './search-users.component.html',
+  styleUrls: ['./search-users.component.css']
+})
+export class SearchUsersComponent implements OnInit {
+  place: string;
+  language: string;
+
+  constructor() { }
+  ngOnInit() { }
+
+  search(place: string, language: string) {
+    this.place = place;
+    this.language = language;
+    console.log(this.place, this.language);
+  }
+}
